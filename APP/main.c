f76ae17b81b87fc2579ea8c18014458d52254b89
Ahmed Mohamed <108755704+AhmedTorres2000@users.noreply.github.com>
2023-01-22 01:54:49 +0200

    Add files via upload

---
+++ APP/main.c
@@ -0,0 +1,52 @@
+/*
+ * main.c
+ *
+ *  Created on: Jan 20, 2023
+ *      Author: Torres
+ */
+#include <util/delay.h>
+#include <avr/interrupt.h>
+
+#include "BIT_MATH.h"
+#include "STD_TYPES.h"
+
+#include "DIO_interface.h"
+#include "SvM_interface.h"
+#include "Ultrasonic_interface.h"
+#include "UART_interface.h"
+#include "GIE_interface.h"
+
+void main(void)
+{
+	u8 itr;
+	GIE_voidEnable();
+	USART_voidInit();
+	SVM_voidServoInit();
+	Ultrasonic_voidInit();
+
+	while(1)
+	{
+		UART_voidSendDecimalNumber(1);
+		/*
+		for(itr=10;itr<170;itr++)
+		{
+			SVM_voidSetAngle2(itr);				// Set Angle
+			_delay_ms(30);
+			UART_voidSendDecimalNumber(itr);		// Send the Angle By Uart Protocol
+			USART_voidSend(',');
+			Ultra_voidTriger();					// Send the Distance By Uart Protocol
+			USART_voidSend('.');
+		}
+
+		for(itr=170;itr>10;itr--)
+		{
+			SVM_voidSetAngle2(itr);				// Set Angle
+			_delay_ms(30);
+			UART_voidSendDecimalNumber(itr);		// Send the Angle By Uart Protocol
+			USART_voidSend(',');
+			Ultra_voidTriger();		          	// Send the Distance By Uart Protocol
+			USART_voidSend('.');
+		}
+		*/
+	}
+}
---
+++ HAL/Ultrasonic_program.c
@@ -0,0 +1,93 @@
+/**************************************************************************************/
+/********************			Author: Ahmed Mohamed			***********************/
+/********************			Layer: HAL			 			***********************/
+/********************			Component: Ultrasonic 		    ***********************/
+/********************			Version: 1.00		 			***********************/
+/********************			Date: 16-12-2022	 			***********************/
+/**************************************************************************************/
+
+#include <util/delay.h>
+
+#include "STD_TYPES.h"
+#include "BIT_MATH.h"
+
+#include "Ultrasonic_config.h"
+#include "Ultrasonic_interface.h"
+
+#include "EXTI_interface.h"
+#include "DIO_interface.h"
+#include "TIMER_interface.h"
+#include "TIMER_register.h"
+#include "UART_interface.h"
+
+u8 sensor_working=0;
+u8 rising_edge=0;
+u32 timer_counter=0;
+u32 distance;
+
+
+
+void Ultrasonic_voidInit(void)
+{
+
+	TIMER0_voidInit();
+
+	DIO_u8SetPinDirection(ECHO_PORT,ECHO_PIN,DIO_u8PIN_INPUT);
+	DIO_u8SetPinValue(ECHO_PORT,ECHO_PIN,DIO_u8PIN_HIGH);
+
+	DIO_u8SetPinDirection(TRIGER_PORT,TRIGER_PIN,DIO_u8PIN_OUTPUT);
+
+	EXTI_voidInt1Init();
+
+}
+
+
+void Ultra_voidTriger(void)
+{
+	if(!sensor_working){
+
+		DIO_u8SetPinValue(TRIGER_PORT,TRIGER_PIN,DIO_u8PIN_HIGH);
+		_delay_us(15);
+		DIO_u8SetPinValue(TRIGER_PORT,TRIGER_PIN,DIO_u8PIN_LOW);
+		sensor_working=1;
+		UART_voidSendDecimalNumber(distance);
+
+	}
+
+}
+
+
+void __vector_2 (void) __attribute__((signal));
+void __vector_2 (void)
+{
+	if(sensor_working==1)
+	{
+		if(rising_edge==0)
+		{
+			TCNT0=0x00;
+			rising_edge=1;
+			timer_counter=0;
+		}
+		else{
+			distance=(timer_counter*256+TCNT0)/466;
+
+			_delay_ms(40);
+			TCNT0 = 0x00;
+			sensor_working=0;
+			timer_counter=0;
+			rising_edge=0;
+		}
+	}
+}
+void __vector_9 (void) __attribute__((signal));
+void __vector_9 (void)
+{
+	timer_counter++;
+	if(timer_counter >550){
+
+		sensor_working=0;
+		rising_edge=0;
+		timer_counter=0;
+	}
+}
+
