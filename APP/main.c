a5c3ae3d242c46305b51f23a71bfc2b3f61bc88a
Ahmed Mohamed <108755704+AhmedTorres2000@users.noreply.github.com>
2023-01-22 02:58:45 +0200

    Update main.c

--- APP/main.c
+++ APP/main.c
@@ -1,11 +1,12 @@
-/*
- * main.c
- *
- *  Created on: Jan 20, 2023
- *      Author: Torres
- */
+/**************************************************************************************/
+/********************		Author: Ahmed Mohamed		***********************/
+/********************		Layer: APP			***********************/
+/********************		Component: MASTER ECU	        ***********************/
+/********************		Version: 1.00		 	***********************/
+/********************		Date: 16-12-2022	 	***********************/
+/**************************************************************************************/
+
 #include <util/delay.h>
-#include <avr/interrupt.h>
 
 #include "BIT_MATH.h"
 #include "STD_TYPES.h"
@@ -26,15 +27,13 @@ void main(void)
 
 	while(1)
 	{
-		UART_voidSendDecimalNumber(1);
-		
 		for(itr=10;itr<170;itr++)
 		{
 			SVM_voidSetAngle2(itr);				// Set Angle
 			_delay_ms(30);
 			UART_voidSendDecimalNumber(itr);		// Send the Angle By Uart Protocol
 			USART_voidSend(',');
-			Ultra_voidTriger();					// Send the Distance By Uart Protocol
+			Ultra_voidTriger();				// Send the Distance By Uart Protocol
 			USART_voidSend('.');
 		}
 
